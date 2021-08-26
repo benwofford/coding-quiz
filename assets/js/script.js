e580b017183e60849596a17f2bb3286e3c64fc43
BenWofford <woffordbt@gmail.com>
2021-08-25 23:17:23 -0400

    wrote up beginning jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
-function startQuiz() {
-    var moveSection1 = document.getElementById("#section-1");
-    if (moveSection1.style.display === "none") {
-        moveSection1.style.display = "block";
-    } else {
-        moveSection1.style.display = "none";
-    }
+var counter = 75;
+var timer = null;
+
+// This function initiates the timer
+function countDown() {
+    counter--;
+    $("#clock").html(counter);
+        timer= setInterval("countDown()", 1000);
 }
+
+
+// This hides the sections at page load until called upon
+$("#section-2").hide(); $("#section-3").hide(); $("#section-4").hide();
+
+//When the start button is clicked this simultaneously calls up the quiz section and starts the timer
+$("#start-btn").click(function() {
+    $("#section-1").hide();
+        $("#section-2").show();
+});
