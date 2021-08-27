77990fd6c9a9bb71c390d5d3189737877153c4ae
BenWofford <woffordbt@gmail.com>
2021-08-26 21:21:06 -0400

    built variables for answers, questions and id's for them in HTML

---
+++ README.md
@@ -0,0 +1,40 @@
+# Coding Quiz #
+
+
+You can access this project here:
+
+## Motivation for Creation ##
+
+--
+## Build Status ##
+
+--
+## Screenshots ##
+
+--
+## Features ##
+
+--
+
+## Code Example ##
+
+--
+
+## Installation ##
+
+--
+## How To Use ##
+
+--
+
+## Contribute ##
+
+--
+
+## Credits ##
+
+--
+
+## License ##
+
+--
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,19 @@
 var counter = 75;
 var timer = null;
 
-// Variables for the questions and answers live here
-var question1 = ""
+// Variables for the questions live here
+var question1 = "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?";
+var question2 = "In JavaScript, what is used in conjunction with HTML to “react” to certain elements?";
+var question3 = "In JavaScript, what is a block of code called that is used to perform a specific task?";
+var question4 = "In JavaScript, what element is used to store and manipulate text usually in multiples?";
+var question5 = "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?";
+
+// Variables for the answers live here
+var answers1 = "Syntax*, scope, output, JSON";
+var answers2 = "Events*, RegExp, Boolean, Condition";
+var answers3 = "String, Declaration, Function*, Variable";
+var answers4 = "Arrays, Strings*, Function, Variables";
+var answers5 = "Conditional Loop, Else Loop, For Loop, While Loop*";
 
 // This function initiates the timer --> needs big fix :(
 function countDown() {
@@ -16,7 +27,7 @@ function countDown() {
                 return;
             }
         }
-}
+};
 
 
 // This hides the sections at page load until called upon
