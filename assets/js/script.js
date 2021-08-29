b4cf5ef689a21010d9e897b8e44c462799eaf7fe
BenWofford <woffordbt@gmail.com>
2021-08-29 19:01:27 -0400

    built out broken JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,25 @@ var questions = [
 
 // Variables for the answers live here
 var answers = [
-  ["Syntax*", "scope", "output", "JSON"],
-  ["Events*", "RegExp", "Boolean", "Condition"],
-  ["String", "Declaration", "Function*", "Variable"],
-  ["Arrays", "Strings*", "Function", "Variables"],
-  ["Conditional Loop", "Else Loop", "For Loop", "While Loop*"],
+  ["Syntax", "scope", "output", "JSON"],
+  ["Events", "RegExp", "Boolean", "Condition"],
+  ["String", "Declaration", "Function", "Variable"],
+  ["Arrays", "Strings", "Function", "Variables"],
+  ["Conditional Loop", "Else Loop", "For Loop", "While Loop"],
 ];
 
 var answerButtons = document.getElementsByClassName("answer-btn");
 
 var questionCount = 0;
+
+// This hides the sections at page load until called upon
+window.onload = function () {
+  document.getElementById("section-2").style.display = "none";
+  document.getElementById("section-3").style.display = "none";
+  document.getElementById("section-4").style.display = "none";
+  document.getElementById("correct").style.display = "none";
+  document.getElementById("wrong").style.display = "none";
+};
 
 // Show sections
 var showSection2 = function () {
@@ -30,22 +39,6 @@ var showSection3 = function () {
 var showSection4 = function () {
   document.getElementById("section-4").style.display = "";
 };
-
-var showQuestion = function() {
-    document.getElementById("question").textContent = questions [questionCount];
-    console.log(document.getElementsByClassName("answer-btn"));
-    for (var i = 0; i < answerButtons.length; i++) {
-        answerButtons[i].textContent = answers[questionCount][i];
-    }
-};
-
-for (var i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].addEventListener("click", function(e){
-        console.log(e.target.textContent);
-        questionCount++
-        showQuestion();
-    })
-}
 
 // Start timer
 var timeleft = 75;
@@ -61,22 +54,58 @@ var countDown = function () {
   }, 1000);
 };
 
-// This hides the sections at page load until called upon
-window.onload = function () {
-  document.getElementById("section-2").style.display = "none";
-  document.getElementById("section-3").style.display = "none";
-  document.getElementById("section-4").style.display = "none";
-};
-
 //When the start button is clicked this simultaneously calls up the quiz section and starts the timer
 function startQuiz() {
   showSection2();
   countDown();
   document.getElementById("section-1").style.display = "none";
   showQuestion();
-}
+};
 
-// Using a data-attribute to point to the correct answer (https://www.sitepoint.com/how-why-use-html5-custom-data-attributes/)
+// This gets a question to populate the h2 tag
+var showQuestion = function() {
+    document.getElementById("question").textContent = questions [questionCount];
+    console.log(document.getElementsByClassName("answer-btn"));
+    for (var i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].textContent = answers[questionCount][i];
+    }
+};
+
+// This gets a set of answers to populate the answer buttons
+for (var i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener("click", function(e){
+        console.log(e.target.textContent);
+        questionCount++
+        showQuestion();
+    })
+};
+
+var dataCorrect = [
+  "Syntax",
+  "Events",
+  "Function",
+  "Strings",
+  "While loop"
+]
+
+// This looks for the correct/wrong answers and calls the corresponding p tag --> needs fix
+ var correctAnswers = function() {
+   
+  }; 
+
+// This stops the quiz after the array finishes and calls up the final sections --> needs fix
+var quizComplete = function() {
+if (answerButtons.length > 5)
+  return;
+  showSection3();
+  showSection4();
+};
+
+// Clicking View Highscores takes you to leaderboard
+
+// Using a data-attribute to point to the correct answer? (https://www.sitepoint.com/how-why-use-html5-custom-data-attributes/)
+
+// Making correct/wrong pop up
 
 // Then getting function(e) to "listen" for that data-attribute
 
